927acd7a4787a29b42e806c9a56bb3b4d646769b
KoksalBerkay <koksalberkaydenktas@gmail.com>
2022-08-03 23:54:54 +0300

    Connecting to the database

--- cmd/web/main.go
+++ cmd/web/main.go
@@ -9,6 +9,7 @@ import (
 	"time"
 
 	"github.com/KoksalBerkay/bookings/internal/config"
+	"github.com/KoksalBerkay/bookings/internal/driver"
 	"github.com/KoksalBerkay/bookings/internal/handlers"
 	"github.com/KoksalBerkay/bookings/internal/helpers"
 	"github.com/KoksalBerkay/bookings/internal/models"
@@ -24,12 +25,13 @@ var errorLog *log.Logger
 
 // main is the entry point for the program
 func main() {
-	err := run()
+	db, err := run()
 	if err != nil {
 		log.Fatal(err)
 	}
+	defer db.SQL.Close()
 
-	fmt.Println(fmt.Sprintf("Starting application on port %s", portNumber))
+	fmt.Printf("Starting application on port %s", portNumber)
 
 	srv := &http.Server{
 		Addr:    portNumber,
@@ -42,7 +44,7 @@ func main() {
 	}
 }
 
-func run() error {
+func run() (*driver.DB, error) {
 
 	// what am I going to put in the session
 	gob.Register(models.Reservation{})
@@ -64,19 +66,27 @@ func run() error {
 
 	app.Session = session
 
+	// connect to database
+	log.Println("Connecting to database...")
+	db, err := driver.ConnectSQL("host=localhost port=5432 user=postgres dbname=bookings password=Kd492372")
+	if err != nil {
+		log.Fatal("Cannot connect to database: ", err)
+	}
+	log.Println("Connected to database!")
+
 	tc, err := render.CreateTemplateCache()
 	if err != nil {
 		log.Fatal("Cannot create template cache")
-		return err
+		return nil, err
 	}
 
 	app.TemplateCache = tc
 	app.UseCache = false
 
-	repo := handlers.NewRepo(&app)
+	repo := handlers.NewRepo(&app, db)
 	handlers.NewHandlers(repo)
 	render.NewTemplates(&app)
 	helpers.NewHelpers(&app)
 
-	return nil
+	return db, nil
 }
